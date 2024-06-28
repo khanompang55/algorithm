938471236c306c0401bb24c01b18eddab0df09be
khanompang55 <674259014@webmail.npru.ac.th>
2024-06-28 10:47:59 +0700

    first commit

---
+++ DisplayText.java
@@ -0,0 +1,9 @@
+public class DisplayText {
+    public static void main(String[] args) {
+    System.out.println("HelloWorld");
+    System.out.println("Peerada" + "Muenreo");
+    System.out.println(10 + 5);
+    System.out.println("NPRU" + 2567);
+    System.out.println(0+"3"+2);
+    }    
+}
